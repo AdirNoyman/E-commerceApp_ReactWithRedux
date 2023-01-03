768e83bf709e60719e5a60bf7bf4082c440855de
AdirNoyman <ofahol2007@hotmail.com>
2023-01-03 19:42:44 +0200

    Fixed stars rating bug

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,22 +1,72 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import {
+	BsStarFill,
+	BsStarHalf,
+	BsStar,
+	BsReverseLayoutSidebarInsetReverse,
+} from 'react-icons/bs';
+const Stars = ({ stars, reviews }) => {
+	console.log(stars, reviews);
+
+	const displayStars = numStars => {
+		let num = numStars;
+		const arr = [];
+		while (num >= 1) {
+			arr.push(1);
+			num -= 1;
+		}
+		if (num > 0) {
+			arr.push(num % 1);
+		}
+		while (arr.length < 5) {
+			let run = 5 - arr.length;
+			while (run > 0) {
+				arr.push(0);
+				run--;
+			}
+		}
+		return arr;
+	};
+
+	const starsArray = displayStars(stars);
+
+	return (
+		<Wrapper>
+			<div className='stars'>
+				{/* star */}
+				<span>
+					{starsArray.map((star, index) => {
+						if (star === 1) {
+							return <BsStarFill key={index} />;
+						} else {
+							return star >= 0.5 ? (
+								<BsStarHalf key={index} />
+							) : (
+								<BsStar key={index} />
+							);
+						}
+					})}
+				</span>
+				{/* end of star */}
+			</div>
+			<p className='reviews'>({reviews} customer reviews)</p>
+		</Wrapper>
+	);
+};
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  span {
-    color: #ffb900;
-    font-size: 1rem;
-    margin-right: 0.25rem;
-  }
-  p {
-    margin-left: 0.5rem;
-    margin-bottom: 0;
-  }
-  margin-bottom: 0.5rem;
-`
-export default Stars
+	display: flex;
+	align-items: center;
+	span {
+		color: #ffb900;
+		font-size: 1rem;
+		margin-right: 0.25rem;
+	}
+	p {
+		margin-left: 0.5rem;
+		margin-bottom: 0;
+	}
+	margin-bottom: 0.5rem;
+`;
+export default Stars;
