6ea971ff0798a57bb73a866c242ec700513cc3ab
AdirNoyman <ofahol2007@hotmail.com>
2023-03-04 20:09:10 +0200

    Added cart persistent to local storage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,18 @@ import {
 	COUNT_CART_TOTALS,
 } from '../actions';
 
+// Check if we have already items in local storage
+const getLocalStorage = () => {
+	let cart = localStorage.getItem('cart');
+	if (cart) {
+		return JSON.parse(localStorage.getItem('cart'));
+	} else {
+		return [];
+	}
+};
+
 const initialState = {
-	cart: [],
+	cart: getLocalStorage(),
 	total_items: 0,
 	total_amount: 0,
 	shipping_fee: 534,
@@ -34,6 +44,10 @@ export const CartProvider = ({ children }) => {
 	const toggleAmount = () => {};
 	// clear cart
 	const clearCart = () => {};
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(state.cart));
+	}, [state.cart]);
 
 	return (
 		<CartContext.Provider
